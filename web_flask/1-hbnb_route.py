03a2767f7969a74e5eb78ee015f3e45e5cb11022
kassem00 <kamsdonga@gmail.com>
2024-03-24 21:13:29 +0000

    au

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home_page(name=None):
+    """fun fact it's home page """
+    return "Hello HBNB!"
+@app.route('/hbnb')
+def hbnb_page(name=None):
+    """fun fact it's hbnb page """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
