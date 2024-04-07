0a4a3cc0a61d816edb32e3b746198aff2f47169e
kassem00 <kamsdonga@gmail.com>
2024-04-07 21:22:01 +0000

    au

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,45 +1,12 @@
 #!/usr/bin/env bash
-# web_static
-
+# web static deploy
 sudo apt-get -y update
 sudo apt-get -y upgrade
-if sudo apt-get -y install nginx; then
-    sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-    echo "test 1234" | sudo tee /data/web_static/releases/test/index.html >/dev/null
-    sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-    sudo chown -hR ubuntu:ubuntu /data/
-
-    sudo echo  -e "
-http {
-     include /etc/nginx/mime.types;
-     default_type application/octet-stream;
-     include /etc/nginx/conf.d/*.conf;
-     include /etc/nginx/sites-enabled/*;
-}
-events {}
-" | sudo tee /etc/nginx/nginx.conf > /dev/null
-sudo echo  -e "
-server {
-       listen 80;
-       location / {
-       root /var/www/;
-       index index.html;                                              
-       }
-}
-" | sudo tee /etc/nginx/sites-available/default > /dev/null
-sudo mkdir -p /var/www/
-sudo echo "Hello World!" | sudo tee /var/www/index.html > /dev/null
-sudo sed -i '8i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo service nginx start
-else
-    echo "Failed to install Nginx."
-    exit 1
-fi
-
-
-
-sudo sed -i "s/http {/http {\n\tadd_header X-Served-By $HOSTNAME;/" /etc/nginx/nginx.conf
-sudo echo "Hello World!" | sudo tee /usr/share/nginx/html/index.html > /dev/null
-sudo service nginx start
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo sed -i "s/http {/http {\n\tadd_header X-Served-By \$HOSTNAME;/" /etc/nginx/nginx.conf
 sudo service nginx restart
