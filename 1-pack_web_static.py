dbe356c4d87975f2a7fdbf8253c031ef0542092a
kassem00 <kamsdonga@gmail.com>
2024-04-07 23:02:55 +0000

    au

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from fabric import Connection
 """
 Fabric script that generates a .tgz archive
 from the contents of the web_static folder of your AirBnB Clone repo,
 using the function do_pack
 """
-
+def do_pack():
+    """ pack to .tgz"""
+    
